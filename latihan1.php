541f7acb38f86196163e1472ed748d984b479bb0
anjen-tyas <anjen1800016010@webmail.uad.ac.id>
2020-06-10 18:09:24 +0700

    Latihan 1 2

---
+++ latihan1.php
@@ -0,0 +1,27 @@
+<?php
+	echo "<right>";
+	echo "<table border>";
+
+	echo "</tr>";
+	echo "<td>NIM</td>";
+	echo "<td>".$_POST['nim']."</td>";
+	echo "</tr>";
+
+	echo "</tr>";
+	echo "<td>Nama</td>";
+	echo "<td>".$_POST['nama']."</td>";
+	echo "</tr>";
+
+	echo "</tr>";
+	echo "<td>Jenis Kelamin</td>";
+	echo "<td>".$_POST['kelamin']."</td>";
+	echo "</tr>";
+
+	echo "</tr>";
+	echo "<td>Program Studi</td>";
+	echo "<td>".$_POST['prodi']."</td>";
+	echo "</tr>";
+
+	echo "</table>";
+	echo "</right>";
+?>
